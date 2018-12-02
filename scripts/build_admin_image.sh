22a58d592e6aa49f41ad895c94f099a30dc2023b
Yirui <yirui.wyr@gmail.com>
2018-12-02 20:07:53 +0800

    Add script for building worker image

--- scripts/build_admin_image.sh
+++ scripts/build_admin_image.sh
@@ -1,1 +1,1 @@
-docker build -t rafiki_admin:0.0.1 -f dockerfiles/admin.Dockerfile --build-arg DOCKER_WORKDIR_PATH=$DOCKER_WORKDIR_PATH .
+docker build -t wangyirui/rafiki_admin:0.0.7 -f dockerfiles/admin.Dockerfile --build-arg DOCKER_WORKDIR_PATH=$DOCKER_WORKDIR_PATH .
---
+++ scripts/build_worker_image.sh
@@ -0,0 +1,1 @@
+docker build -t wangyirui/rafiki_worker:0.0.7 -f dockerfiles/worker.Dockerfile --build-arg DOCKER_WORKDIR_PATH=$DOCKER_WORKDIR_PATH .
